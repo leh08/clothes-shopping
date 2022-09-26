dd963a401b6ab6b40b5cd481075ec238a39bb187
Giang Le <37028078+leh08@users.noreply.github.com>
2022-09-26 15:03:42 +0700

    Initial commit

---
+++ project/app/migrations/models/0_20220926064446_init.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> List[str]:
+    return [
+        """CREATE TABLE IF NOT EXISTS "textsummary" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "url" TEXT NOT NULL,
+    "summary" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSONB NOT NULL
+);"""
+    ]
+
+
+async def downgrade(db: BaseDBAsyncClient) -> List[str]:
+    return [
+        
+    ]
